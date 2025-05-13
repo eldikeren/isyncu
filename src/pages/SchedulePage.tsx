c0022060a3d26667583b59d601f435513899afef
Eldi Keren <elad.d.keren@gmail.com>
2025-05-13 11:18:15 +0300

    Fix: corrected inline style in SchedulePage

--- src/pages/SchedulePage.tsx
+++ src/pages/SchedulePage.tsx
@@ -3,7 +3,8 @@ import SchedulingAssistant from '../components/scheduling/SchedulingAssistant';
 
 const SchedulePage = () => {
   return (
-    <div style={ padding: '2rem' }>
+  <div style={{ padding: '2rem' }}>
+
       <h1>Schedule Rehearsals</h1>
       <SchedulingAssistant />
     </div>
